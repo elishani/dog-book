52cc3cfafd4a23b8e031429543988f6c34cdeaa3
Eli Shani <elishani50@gmail.com>
2020-01-21 12:46:24 +0200

    Add router and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import BreedPage from './pages/BreedPage.js';
+import DogsPage from './pages/DogsPage.js';
 
 class App extends React.Component{
   render(){
     return(
-      <h1>App</h1>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <HomePage/>
+          </Route>
+          <Route exact path="/breed">
+            <BreedPage/>
+          </Route>
+          <Route exact path="/dogs">
+            <DogsPage/>
+          </Route>
+        </Switch>
+      </HashRouter>
     )
   }
 }
---
+++ src/pages/DogsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DogsPage extends React.Component{
+    render(){
+        return(
+            <h1>DogsPage</h1>
+        )
+    }
+}
+
+export default DogsPage
